80bac182f24fef2fde7c747a707fa26d37a898f4
Max Alva <alva85@gmail.com>
2021-10-30 17:20:59 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,13 @@
+require( 'dotenv' ).config( {
+  path: `.env.${process.env.NODE_ENV}`,
+} );
+
+module.exports = ({ env }) => ({
+  host: env('HOST', process.env.HOST),
+  port: env.int('PORT', process.env.PORT),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '038fa735-6ca8-4af0-9c06-2e31b005b26a'
+};
